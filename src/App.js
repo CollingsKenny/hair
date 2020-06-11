bbfe00af86ee95589f0b170192560974b0d7fea0
Kenny <keacolli@ucsc.edu>
2020-06-11 11:59:50 -0700

    add addItems

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { getItemInfo } from './products.js';
 
 import csv from 'csv';
 import * as FileSaver from 'file-saver';
@@ -9,6 +10,7 @@ function App() {
   const [orders, setOrders] = useState(null);
   const [hairTypes, setHairTypes] = useState(null);
   const [output, setOutput] = useState(null);
+  const [processText, setProcessText] = useState('Process');
 
   const csvOptions = {
     bom: true,
@@ -50,19 +52,32 @@ function App() {
     fileReader.readAsBinaryString(file);
   };
 
-  const addHairToOrders = () => {
-    setOutput(
-      orders.map((order) => {
-        return {
-          ...order,
-          itemOptions: getProductOption(
-            hairTypes.find(
-              (hairType) => hairType['Order Number'] == order['ï»¿Order #']
-            )
-          ),
-        };
-      })
-    );
+  const addHairToOrders = (input) => {
+    return input.map((order) => {
+      return {
+        ...order,
+        itemOptions: getProductOption(
+          hairTypes.find(
+            (hairType) => hairType['Order Number'] === order['ï»¿Order #']
+          )
+        ),
+      };
+    });
+  };
+
+  const addItemInfo = (input) => {
+    return input.map((order) => {
+      return {
+        ...order,
+        ...getItemInfo(order["Item's Name"]),
+      };
+    });
+  };
+
+  const processOrders = () => {
+    setProcessText('Processing...');
+    setOutput(addItemInfo(addHairToOrders(orders)));
+    setProcessText('Done!');
   };
 
   const onOutput = () => {
@@ -81,15 +96,15 @@ function App() {
     );
   };
 
-  const mapReduce = (data) => {
-    let keys = data.shift();
-    return data.map((row) => {
-      return keys.reduce((obj, key, i) => {
-        obj[key] = row[i];
-        return obj;
-      }, {});
-    });
-  };
+  // const mapReduce = (data) => {
+  //   let keys = data.shift();
+  //   return data.map((row) => {
+  //     return keys.reduce((obj, key, i) => {
+  //       obj[key] = row[i];
+  //       return obj;
+  //     }, {});
+  //   });
+  // };
 
   return (
     <div className='App'>
@@ -111,15 +126,24 @@ function App() {
           onChange={(e) => onHairChange(e.target.files[0])}
         />
       </div>
-      <button onClick={(e) => addHairToOrders()}>Add Hair to Orders</button>
-      <button onClick={(e) => onOutput()}>Output</button>
-      <button onClick={(e) => console.log(output)}> View Output</button>
+      <div>
+        <button onClick={(e) => console.log(output)}> View Output</button>
 
-      <button onClick={(e) => console.log(hairTypes[0]['Order Number'])}>
-        test hair types id
+        <button onClick={(e) => console.log(hairTypes[0]['Order Number'])}>
+          test hair types id
+        </button>
+        <button onClick={(e) => console.log(orders[0]['ï»¿Order #'])}>
+          test order id
+        </button>
+      </div>
+      <button
+        onClick={(e) => processOrders()}
+        disabled={!(hairTypes && orders)}
+      >
+        {processText}
       </button>
-      <button onClick={(e) => console.log(orders[0]['ï»¿Order #'])}>
-        test order id
+      <button onClick={(e) => onOutput()} disabled={!output}>
+        Download
       </button>
     </div>
   );
--- src/products.js
+++ src/products.js
@@ -8,3 +8,37 @@ const Two_Treatments = {
     h: 1,
   },
 };
+
+const Single_Treatment = {
+  name: 'Single Treatment',
+  price: '67',
+  weight: 7,
+  dimensions: {
+    l: 6,
+    w: 9,
+    h: 1,
+  },
+};
+
+export function getItemInfo(itemName) {
+  switch (itemName) {
+    case 'Buy 2 Treatments - Save 43%':
+      return {
+        ItemWeight: Two_Treatments.weight,
+        ItemLength: Two_Treatments.dimensions.l,
+        ItemWidth: Two_Treatments.dimensions.w,
+        ItemHeight: Two_Treatments.dimensions.h,
+      };
+    case 'Single Treatment':
+      return {
+        ItemWeight: Single_Treatment.weight,
+        ItemLength: Single_Treatment.dimensions.l,
+        ItemWidth: Single_Treatment.dimensions.w,
+        ItemHeight: Single_Treatment.dimensions.h,
+      };
+
+    default:
+      console.log('No item found for: ', itemName);
+      return {};
+  }
+}
