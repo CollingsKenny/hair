8db279f8815cacedf5113731c50715779f3f84c5
Kenny <keacolli@ucsc.edu>
2020-06-11 10:11:33 -0700

    Updates

---
+++ precache-manifest.ddac2be1c682aa742912e16e13b8af18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd576258dc6f21686091cbdb47dea62",
+    "url": "/hair/index.html"
+  },
+  {
+    "revision": "07752cbebd669ff1d88f",
+    "url": "/hair/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd21dbe6a76e143d3be6",
+    "url": "/hair/static/js/2.b62c08f4.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/hair/static/js/2.b62c08f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07752cbebd669ff1d88f",
+    "url": "/hair/static/js/main.cc95f9d2.chunk.js"
+  },
+  {
+    "revision": "6dc1b7e5158547fa0b45",
+    "url": "/hair/static/js/runtime-main.d923d234.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hair/static/media/logo.5d5d9eef.svg"
+  }
+]);
