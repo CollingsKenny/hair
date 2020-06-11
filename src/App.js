15eab4dfae2be8618ec7b055ad026cee657a6921
Kenny <keacolli@ucsc.edu>
2020-06-11 10:06:31 -0700

    add ability to combine hair types and orders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import csv from 'csv';
+import * as FileSaver from 'file-saver';
+
 function App() {
+  const [orders, setOrders] = useState(null);
+  const [hairTypes, setHairTypes] = useState(null);
+  const [output, setOutput] = useState(null);
+
+  const csvOptions = {
+    bom: true,
+    columns: true,
+  };
+
+  const getProductOption = (hairType) => {
+    if (hairType) {
+      const long = hairType['Hair Length'] === 'long' ? 'long' : '';
+      const formula = hairType['Hair Type'];
+      if (formula === 'ultra') {
+        return long + formula;
+      }
+      return formula;
+    }
+
+    return 'none';
+  };
+
+  const onOrdersChange = (file) => {
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+      csv.parse(fileReader.result, csvOptions, (err, data) => {
+        setOrders(data);
+        //setOrders(mapReduce(data));
+      });
+    };
+    fileReader.readAsBinaryString(file);
+  };
+
+  const onHairChange = (file) => {
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+      csv.parse(fileReader.result, csvOptions, (err, data) => {
+        setHairTypes(data);
+        //setOrders(mapReduce(data));
+      });
+    };
+    fileReader.readAsBinaryString(file);
+  };
+
+  const addHairToOrders = () => {
+    setOutput(
+      orders.map((order) => {
+        return {
+          ...order,
+          itemOptions: getProductOption(
+            hairTypes.find(
+              (hairType) => hairType['Order Number'] == order['ï»¿Order #']
+            )
+          ),
+        };
+      })
+    );
+  };
+
+  const onOutput = () => {
+    csv.stringify(
+      output,
+      { header: true, columns: Object.keys(output[0]) },
+      (err, data) => {
+        console.log(data);
+        FileSaver.saveAs(
+          new Blob([data], {
+            type: 'text/csv;charset=utf-8',
+          }),
+          'output.csv'
+        );
+      }
+    );
+  };
+
+  const mapReduce = (data) => {
+    let keys = data.shift();
+    return data.map((row) => {
+      return keys.reduce((obj, key, i) => {
+        obj[key] = row[i];
+        return obj;
+      }, {});
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <h2 onClick={(e) => console.log(orders)}>Orders</h2>
+        <input
+          type='file'
+          id='file'
+          accept='.csv'
+          onChange={(e) => onOrdersChange(e.target.files[0])}
+        />
+      </div>
+      <div>
+        <h2 onClick={(e) => console.log(hairTypes)}>Hair_type</h2>
+        <input
+          type='file'
+          id='file'
+          accept='.csv'
+          onChange={(e) => onHairChange(e.target.files[0])}
+        />
+      </div>
+      <button onClick={(e) => addHairToOrders()}>Add Hair to Orders</button>
+      <button onClick={(e) => onOutput()}>Output</button>
+      <button onClick={(e) => console.log(output)}> View Output</button>
+
+      <button onClick={(e) => console.log(hairTypes[0]['Order Number'])}>
+        test hair types id
+      </button>
+      <button onClick={(e) => console.log(orders[0]['ï»¿Order #'])}>
+        test order id
+      </button>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,10 @@
+const Two_Treatments = {
+  name: 'Buy 2 Treatments - Save 43%',
+  price: '77',
+  weight: 12,
+  dimensions: {
+    l: 8.5,
+    w: 12,
+    h: 1,
+  },
+};
